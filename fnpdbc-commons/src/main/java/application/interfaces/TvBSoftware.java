c7a0bc0931ba67b2353629572182604a1b29359c
TvBeukelen <van_breukelen@web.de>
2022-07-20 09:29:16 +0200

    DBConvert 7.2.2

--- fnpdbc-commons/src/main/java/application/interfaces/TvBSoftware.java
+++ fnpdbc-commons/src/main/java/application/interfaces/TvBSoftware.java
@@ -1,7 +1,5 @@
 package application.interfaces;
 
-import java.time.Year;
-
 public enum TvBSoftware {
 	/**
 	 * Title: TvBSoftware Description: Enums for version and support information
@@ -10,7 +8,7 @@ public enum TvBSoftware {
 	 * @author Tom van Breukelen
 	 */
 
-	DBCONVERT("DBConvert", "7.2.1", "dbconvert", "dbconvert/files", "dbconvert/best_release.json"),
+	DBCONVERT("DBConvert", "7.2.2", "dbconvert", "dbconvert/files", "dbconvert/best_release.json"),
 	FNPROG2PDA("FNProg2PDA", "9.7", "fnprog2pda", "fnprog2pda/files", "fnprog2pda/best_release.json");
 
 	private static final String SOURCEFORGE = "https://sourceforge.net/projects/";
@@ -38,7 +36,7 @@ public enum TvBSoftware {
 	}
 
 	public String getCopyright() {
-		return "Copyright 2003-" + Year.now().getValue() + " Tom van Breukelen.";
+		return "Copyright 2003-2022 Tom van Breukelen.";
 	}
 
 	public String getSupport() {
